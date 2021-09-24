ac2478016569dcbdf37133af0c9132645f323f62
abdurrazaq <razaqofficial@gmail.com>
2021-09-24 21:58:05 +0100

    updated config

--- backend-ex/pm2.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps : [{
-    name      : 'UdaPeople',
-    script    : '/home/ubuntu/backend/dist/main.js',
-    exec_mode : 'cluster_mode',
-    node_args : '-r dotenv/config'
-  }]
-};
